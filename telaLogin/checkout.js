b1149a59792e52036b932ae476f3388569b85bf5
Rafael <rafael.oliveiratonon@yahoo.com>
2023-05-23 21:09:08 -0300

    Criação recuperação de senha
    Criei duas páginas referente a recuperação de senhas, falta linkar a "esqueceu sua senha" a página "esqueciminhasenha.html", linkar a segunda a pagina "retorno", e, por fim, linkar está a página incial, todas as ultimas através dos botões.

--- telaLogin/checkout.js
+++ telaLogin/checkout.js
@@ -16,4 +16,19 @@
       form.classList.add('was-validated')
     }, false)
   })
+  type=("text/javascript");
+  var primeiro_ano = prompt('Digite seu ano de nascimento:');
+  var segundo_ano = prompt('Digite seu ano de nascimento:');
+  var terceiro_ano = prompt('Digite seu ano de nascimento:');
+  
+  
+  if((primeiro_ano > segundo_ano) && (primeiro_ano > terceiro_ano)) mensagem = "O primeiro usuário é mais velho!"
+  else
+  if((segundo_ano > primeiro_ano) && (segundo_ano> terceiro_ano)) mensagem=  "O segundo usuário é mais velho!"
+  else
+  if((terceiro_ano>primeiro_ano) && (terceiro_ano > segundo_ano)) mensagem= "O terceiro usuário é mais velho!"
+  else;
+  
+  alert(mensagem);
+
 })()
